e0c0b26a8be9db031cf5e919218f69e42da0a37d
Maria geraseva <Maria_Geraseva@epam.com>
2024-05-29 18:49:18 +0200

    Reverse.java

--- src/main/java/tasks/Reverse.java
+++ src/main/java/tasks/Reverse.java
@@ -1,8 +1,91 @@
 package tasks;
 
 public class Reverse {
+//    Topics to refresh:
 
+//    1.Mutable and immutable instances (String, StringBuilder, array, collections)
+
+//    2.String
+//    charAt(int): Returns char value at index.
+//    length(): Returns string length.
+//    substring(int, int): Returns substring for given begin and end index.
+//    substring(int): Returns substring from beginning to end.
+//    contains(CharSequence): Returns true if sequence of char values are present.
+//    indexOf(int): Returns index for given char value.
+//    indexOf(String): Returns index for given substring.
+//    isEmpty(): Checks if string is empty.
+//    trim(): Removes leading and trailing spaces.
+//    replace(char, char): Replaces old char with new char.
+//    replace(CharSequence, CharSequence): Replaces all occurrences of the specified CharSequence.
+//    split(String): Splits the string by given regular expression.
+//    toLowerCase(): Returns a string in lowercase.
+//    toUpperCase(): Returns a string in uppercase.
+//    valueOf(data type): Converts different types of data into string.
+//    equals(Object): Compares the content of two given strings.
+//    equalsIgnoreCase(String): Compares two strings irrespective of case.
+
+//    3.StringBuilder
+//    append() : Appends string representation of a value.
+//    insert(int, String) : Inserts string at specified offset.
+//    delete(int, int) : Removes characters in specific substring.
+//    deleteCharAt(int) : Removes character at index.
+//    reverse() : Reverses StringBuilder.
+//    replace(int, int, String) : Replaces substring with a string.
+//    ensureCapacity(int) : Ensures minimum capacity.
+//    setLength(int) : Sets the sequence's length.
+//    charAt(int) : Returns character at index.
+//    indexOf(String) : Returns first occurrence index of substring.
+//    lastIndexOf(String) : Returns last occurrence index of substring.
+//    substring(int) : Returns a new String of subsequence of characters.
+//    toString() : Converts StringBuilder to String.
+//    length() : Returns characters count.
+//    capacity() : Returns current capacity.
+
+//    4.Array - https://www.baeldung.com/java-arrays-guide
+//    Arrays.copyOf(): Returns a copy of the given array.
+//    Arrays.equals(array1, array2): Check if two arrays are equal.
+//    Arrays.fill(): Set all elements of array to a specific value.
+//    Arrays.sort(): Sort array elements.
+//    Arrays.binarySearch(): Uses binary search algorithm to find an element.
+//    Arrays.toString(): Returns string representation of an array.
+//    Arrays.asList(): Returns a fixed-size list backed by the specified array.
+//    Arrays.hashCode(): Returns the hash code value for the content of the array.
+//    Arrays.parallelSort(): Perform parallel sort-operations.
+//    Arrays.deepEquals(): Check if two arrays deeply equal to one another.
+//    Arrays.deepHashCode(): Returns the deep hash code value for the array.
+//    Arrays.deepToString(): Returns string representation of a deep array.
+
+//    5.Collections.reverse(List list)
+
+//    6.stream api .collect
+//    stream.collect(Collectors.toList()): Returns a List containing the elements of the stream.
+//    stream.collect(Collectors.toSet()): Returns a Set containing the elements of the stream.
+//    stream.collect(Collectors.toCollection(collectionFactory)): Returns a Collection created by the provided factory method, filled with the elements of the stream.
+//    stream.collect(Collectors.joining(delimiter)): Returns a String consisting of the stream's elements concatenated using the specified delimiter.
+//    stream.collect(Collectors.counting()): Returns the count of the elements in the stream.
+//    stream.collect(Collectors.averagingInt(valueMapper)): Returns an average of Integer values in the stream,
+
+    /**
+     * reverse string
+     * provide 2 solutions
+     */
     public static String reverseString(String stringToReverse) {
        return null;
+    }
+
+    /**
+     * reverse int[]
+     * provide 2 solutions
+     */
+    public static int[] reverseArray(int[] arrayToReverse) {
+        return null;
+    }
+
+    /**
+     * reverse word order in sentence
+     * provide 2 solutions
+     */
+    public static int[] reverseWordOrder(String sentenceToRevert) {
+        return null;
     }
 }
